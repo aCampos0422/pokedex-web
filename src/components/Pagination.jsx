056a04963b944f78d4a6f4051566d74506a1a6b8
Aldo Campos <134451730+aCampos0422@users.noreply.github.com>
2023-08-04 16:41:14 -0600

    pokedex-final

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,11 +1,67 @@
 
 
-const Pagination = () => {
+const Pagination = ({pokesPerPage, currentPage, setCurrentPage, pokemons}) => {
+
+  const pageNumbers = []
+
+
+  for (let i = 1; i <= Math.ceil(1081 / pokesPerPage); i++) {
+    pageNumbers.push(i)
+  }
+
+  const onPreviusPage = () => {
+    setCurrentPage(currentPage - 1)
+  }
+
+  const onNextPage = () => {
+    setCurrentPage(currentPage + 1)
+  }
+
+  const onSpecificPage = (n) => {
+    setCurrentPage(n)
+  }
+
   return (
-    <div>
-      <h2>hola esta es la paginacion</h2>
-    </div>
+    <nav
+      className="pagination is-centered mb-4 mt-4 column"
+      role="navigation"
+      aria-label="pagination"
+    >
+      <button className={`pagination-previous ${currentPage === 1 ? 'is-disabled' : ''}`} onClick={onPreviusPage}>Previous</button>
+      <button className={`pagination-next ${currentPage >= pageNumbers.length ? 'is-disabled' : ''}`} onClick={onNextPage}>Next page</button>
+      <ul className="pagination-list">
+        {
+          pageNumbers?.map(noPage =>(
+            <li key={noPage}>
+              <a 
+                className={`pagination-link ${noPage === currentPage ? 'is-current' : ''}`}
+                onClick={() => onSpecificPage(noPage)}
+              >
+                {noPage}
+              </a>
+            </li>
+          ))
+        }  
+        <li><span class="pagination-ellipsis">&hellip;</span></li>
+      </ul>
+    </nav>
   )
 }
 
-export default Pagination
+export default Pagination;
+
+
+
+{/* <nav class="pagination is-centered" role="navigation" aria-label="pagination">
+  <a class="pagination-previous">Previous</a>
+  <a class="pagination-next">Next page</a>
+  <ul class="pagination-list">
+    <li><a class="pagination-link" aria-label="Goto page 1">1</a></li>
+    <li><span class="pagination-ellipsis">&hellip;</span></li>
+    <li><a class="pagination-link" aria-label="Goto page 45">45</a></li>
+    <li><a class="pagination-link is-current" aria-label="Page 46" aria-current="page">46</a></li>
+    <li><a class="pagination-link" aria-label="Goto page 47">47</a></li>
+    <li><span class="pagination-ellipsis">&hellip;</span></li>
+    <li><a class="pagination-link" aria-label="Goto page 86">86</a></li>
+  </ul>
+</nav> */}
--- src/pages/PokedexPage.jsx
+++ src/pages/PokedexPage.jsx
@@ -9,19 +9,29 @@ import Pagination from "../components/Pagination"
 
 
 export default function PokedexPage() {
-
+  
   const [inputValue, setInputValue]  = useState('')
   const [selectValue, setSelectValue] = useState('allPokemons')
 
   // ESTADO PARA RENDERIZAR LA CANTIDAD DE POKEMONES POR PAGINA
-  const [pokesPerPage, setPokesPerPage] = useState(8)
+  const [pokesPerPage, setPokesPerPage] = useState(20)
+  
+  // ESTADO DE PAGINA ACTUAL
+  const [currentPage, setCurrentPage] = useState(1)
 
+  const lastIndex = currentPage * pokesPerPage
+  const firstIndex = lastIndex - pokesPerPage
+
+
+
+
+  
   const trainer = useSelector(reducer => reducer.trainer)
-
-  const url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=200'
-
+  
+  const url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=1081'
+  
   const [ pokemons, getAllPokemons, getPokemonsByType ] = useFetch(url)
-
+  
   useEffect(()=> {
     if (selectValue === 'allPokemons') {
       getAllPokemons()
@@ -30,14 +40,15 @@ export default function PokedexPage() {
     }
   },[selectValue])
   
+  
   const inputSearch = useRef()
-
+  
   const handleSubmit = e => {
     e.preventDefault()
     setInputValue(inputSearch.current.value.trim().toLowerCase())
     setSelectValue('allPokemons')
   }
-
+  
   const cbFilter = (poke) => poke.name.includes(inputValue)  
 
   return (
@@ -56,8 +67,7 @@ export default function PokedexPage() {
                 setSelectValue={setSelectValue}
                 setInputValue={setInputValue}
                 selectValue={selectValue}
-               
-            />
+                />
           </div>
         </section> 
 
@@ -67,13 +77,18 @@ export default function PokedexPage() {
         {
           pokemons?.results.filter(cbFilter).map(poke => (
             <PokeCard
-              key={poke.url}
-              url={poke.url}
+            key={poke.url}
+            url={poke.url}
             />
-          ) )
-        }
+            )).slice(firstIndex, lastIndex)
+          }
       </div>
-      <Pagination/>
+      <Pagination
+        pokesPerPage={pokesPerPage}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        pokemons={pokemons}
+      />  
     </article>
   )
 }
